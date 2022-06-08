99745b08e919d68a4a17a34673dcab5fefff3b9e
Andi Rustianto <anditodev46@gmail.com>
2022-06-08 22:38:55 +0700

    initial project

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Armstrong on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
