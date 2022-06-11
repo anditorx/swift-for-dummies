615ba4605bb2d0fd269be749be8d2e0cfd62c056
Andi Rustianto <anditodev46@gmail.com>
2022-06-12 01:52:18 +0700

    Add: Latinan statik table view

---
+++ 04-Latihan-Static-Table-View/MySimpleHeroApp/MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Armstrong on 12/06/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
